45537ff3850920d2c662c0dd7b37ad676307ccf0
Vincetroid <vince_trance@hotmail.com>
2019-06-25 23:31:04 -0500

    Oauth authorized redirect URI

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GoogleStrategy = require('passport-google-oauth20'); //la nueva
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
-});
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback',
+        }, (accessToken) => {
+            console.log(accessToken);
+        }
+    )
+);
+
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,3 +32,5 @@ console.log('app');
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
+
+// http://localhost:5000/auth/google/callback
