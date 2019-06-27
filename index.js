e447f4ce65850309728f9082649672c5342e5329
Vincetroid <vince_trance@hotmail.com>
2019-06-26 22:48:36 -0500

    Almost oauth

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const passport = require('passport');
-// const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const GoogleStrategy = require('passport-google-oauth20'); //la nueva
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const GoogleStrategy = require('passport-google-oauth20'); //la nueva
 const keys = require('./config/keys');
 
 const app = express();
@@ -12,8 +12,12 @@ passport.use(
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
             callbackURL: '/auth/google/callback',
-        }, (accessToken) => {
-            console.log(accessToken);
+        }, 
+        (accessToken, refreshToken, profile, done) => {
+            console.log('access token: ' + accessToken);
+            console.log('refresh token: ' + refreshToken);
+            console.log('profile:');
+            console.log(profile);
         }
     )
 );
@@ -24,6 +28,8 @@ app.get(
         scope: ['profile', 'email']
     })
 );
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 
